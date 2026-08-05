8b2c75db8d1717e980cd1b4d30858c294e9a0b7b
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-05 10:18:33 +0330

    Adding some constants to the constants configuration file of the project

---
+++ config/constants.py
@@ -0,0 +1,22 @@
+SUPPORTED_LANGUAGES = [
+    "German",
+    "English",
+]
+
+DEFAULT_LANGUAGE = SUPPORTED_LANGUAGES[0]
+
+SUPPORTED_LEVELS = (
+    "A1",
+    "A2",
+    "B1",
+    "B2",
+    "C1",
+    "C2",
+)
+
+WORD_TYPES = (
+    "Noun",
+    "Verb",
+    "Adjective",
+    "Phrase"
+)
