4e155b978e2dac04730ce7b2d9d0a0e485a613f1
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-05 10:12:44 +0330

    Implementing the paths to my project

---
+++ config/paths.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+# Depth 1
+
+DATA = ROOT / "data"
+LOG = ROOT / "log"
+SRC = ROOT / "src"
+CONFIG = ROOT / "config"
+
+# Depth 2
+
+CHECKLIST = LOG / "checklist"
+DATABASE = SRC / "database"
+
+# Depth 3
+
+DATABASE_SOURCE_CODE = DATABASE / "database.py"
+DATABASE_SCHEM = DATABASE / "schema.sql"
