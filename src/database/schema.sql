66e2ccd9a856e5035b49ba08a7d77438580217c3
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-06 10:51:17 +0330

    Checking the level of the text to be in the supported ones as well.

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS vocabulary (
     article TEXT NOT NULL CHECK (article IN ('der', 'die', 'das')),
     plural TEXT,
 
-    level TEXT,
+    level TEXT CHECK (level IN ('A1', 'A2', 'B1', 'B2', 'C1', 'C2')),
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
