772798620f1e1764df6e70dbdf8159c0d770dc3c
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-13 08:09:12 +0330

    Updating the paths configuration files

--- config/paths.py
+++ config/paths.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
+
 ROOT = Path(__file__).resolve().parent.parent
+
 
 # Depth 1
 
@@ -9,18 +11,25 @@ LOG = ROOT / "log"
 SRC = ROOT / "src"
 CONFIG = ROOT / "config"
 HISTORY = ROOT / "history"
+TEMPLATES = DATA / "templates"
+
 
 # Depth 2
 
 CHECKLIST = LOG / "checklist"
 DATABASE = SRC / "database"
 VOCABULARY = SRC / "vocabulary"
+
 SQLITE_DATABASE = DATA / "german.db"
+
 SESSIONS_HISTORY = HISTORY / "sessions.json"
 ARTICLE_HISTORY = HISTORY / "article_practice_history.json"
 ENGLISH_HISTORY = HISTORY / "english_practice_history.json"
 GERMAN_HISTORY = HISTORY / "german_practice_history.json"
 PLURAL_HISTORY = HISTORY / "plural_practice_history.json"
+
+QUIZ_TEMPLATES = TEMPLATES / "quiz_templates.json"
+
 
 # Depth 3
 
