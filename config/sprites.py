dc51e8060320288dfecc43e966672ff535a69249
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-15 23:38:24 +0330

    Updating the fox art

--- config/sprites.py
+++ config/sprites.py
@@ -1,170 +1,74 @@
-FOX_FRAMES = [
-    r"""
-      {ORANGE}/\_/\\{RESET}
-     {ORANGE}( {WHITE}o.o{ORANGE} ){RESET}
-     {ORANGE}/  >  \{RESET}
-    {DARK_ORANGE}/|     |\{RESET}
-     {ORANGE}\/|___|\/{RESET}
-      {DARK_ORANGE}/   \{RESET}
-     {ORANGE}/_____\{RESET}
-""",
-
-    r"""
-      {ORANGE}/\_/\\{RESET}
-     {ORANGE}( {WHITE}o.o{ORANGE} ){RESET}
-     {ORANGE}/  >  \{RESET}
-    {DARK_ORANGE}/|     |\{RESET}
-     {ORANGE}\/|___|\/{RESET}
-       {DARK_ORANGE}/  \{RESET}
-      {ORANGE}/____\{RESET}
-""",
-
-    r"""
-       {ORANGE}/\_/\\{RESET}
-      {ORANGE}( {WHITE}^.^{ORANGE} ){RESET}
-      {ORANGE}/  >  \{RESET}
-     {DARK_ORANGE}/|     |\{RESET}
-      {ORANGE}\/|___|\/{RESET}
-       {DARK_ORANGE}/   \{RESET}
-      {ORANGE}/_____\{RESET}
-""",
-
-    r"""
-       {ORANGE}/\_/\\{RESET}
-      {ORANGE}( {WHITE}o.o{ORANGE} ){RESET}
-      {ORANGE}/  >  \{RESET}
-     {DARK_ORANGE}/|     |\{RESET}
-      {ORANGE}\/|___|\/{RESET}
-        {DARK_ORANGE}/ \{RESET}
-       {ORANGE}/___\{RESET}
-""",
-
-    r"""
-      {ORANGE}/\_/\\{RESET}
-     {ORANGE}( {WHITE}o.o{ORANGE} ){RESET}
-     {ORANGE}/  >  \{RESET}
-    {DARK_ORANGE}/|     |\{RESET}
-     {ORANGE}\/|___|\/{RESET}
-      {DARK_ORANGE}/   \{RESET}
-     {ORANGE}/_____\{RESET}
-""",
-
-    r"""
-      {ORANGE}/\_/\\{RESET}
-     {ORANGE}( {WHITE}^.^{ORANGE} ){RESET}
-     {ORANGE}/  >  \{RESET}
-    {DARK_ORANGE}/|     |\{RESET}
-     {ORANGE}\/|___|\/{RESET}
-       {DARK_ORANGE}/  \{RESET}
-      {ORANGE}/____\{RESET}
-""",
-
-    r"""
-       {ORANGE}/\_/\\{RESET}
-      {ORANGE}( {WHITE}o.o{ORANGE} ){RESET}
-      {ORANGE}/  >  \{RESET}
-     {DARK_ORANGE}/|     |\{RESET}
-      {ORANGE}\/|___|\/{RESET}
-       {DARK_ORANGE}/   \{RESET}
-      {ORANGE}/_____\{RESET}
-""",
-
-    r"""
-      {ORANGE}/\_/\\{RESET}
-     {ORANGE}( {WHITE}-.-{ORANGE} ){RESET}
-     {ORANGE}/  >  \{RESET}
-    {DARK_ORANGE}/|     |\{RESET}
-     {ORANGE}\/|___|\/{RESET}
-      {DARK_ORANGE}/   \{RESET}
-     {ORANGE}/_____\{RESET}
-""",
-]
+SEAL_ART = r"""
+                                                                   ,-,
+                                                             _.-=;~ /_
+                                                          _-~   '     ;.
+                                                      _.-~     '   .-~-~`-._
+                                                _.--~~:.             --.____88
+                              ____.........--~~~. .' .  .        _..-------~~
+                     _..--~~~~               .' .'             ,'
+                 _.-~                        .       .     ` ,'
+               .'                                    :.    ./
+             .:     ,/          `                   ::.   ,'
+           .:'     ,(            ;.                ::. ,-'
+          .'     ./'.`.     . . /:::._______.... _/:.o/
+         /     ./'. . .)  . _.,'               `88;?88|
+       ,'  . .,/'._,-~ /_.o8P'                  88P ?8b
+    _,'' . .,/',-~    d888P'                    88'  88|
+ _.'~  . .,:oP'        ?88b              _..--- 88.--'8b.--..__
+:     ...' 88o __,------.88o ...__..._.=~- .    `~~   `~~      ~-._ Seal _.
+`.;;;:='    ~~            ~~~                ~-    -       -   -
+"""
 
 
-RACCOON_FRAMES = [
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}o.o{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-      {DARK_GRAY}/  |  \{RESET}
-     {GRAY}/___|___\{RESET}
-""",
+def shift_art(art: str, spaces: int = 0, vertical: int = 0) -> str:
+    """Shift ASCII art horizontally and vertically."""
 
-    r"""
-        {GRAY}/\___/\{RESET}
-       {GRAY}( {WHITE}o.o{GRAY} ){RESET}
-       {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-      {DARK_GRAY}/ |     | \{RESET}
-        {GRAY}/|___|\{RESET}
-       {DARK_GRAY}/  |  \{RESET}
-      {GRAY}/___|___\{RESET}
-""",
+    horizontal = " " * spaces
+    vertical_padding = "\n" * vertical
 
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}^.^{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-        {DARK_GRAY}/ | \{RESET}
-       {GRAY}/__|__\{RESET}
-""",
+    lines = art.splitlines()
 
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}-.-{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-      {DARK_GRAY}/  |  \{RESET}
-     {GRAY}/___|___\{RESET}
-""",
+    shifted_lines = [
+        horizontal + line if line.strip() else line
+        for line in lines
+    ]
 
-    r"""
-        {GRAY}/\___/\{RESET}
-       {GRAY}( {WHITE}o.o{GRAY} ){RESET}
-       {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-      {DARK_GRAY}/ |     | \{RESET}
-        {GRAY}/|___|\{RESET}
-       {DARK_GRAY}/ |   \{RESET}
-      {GRAY}/__|____\{RESET}
-""",
+    return vertical_padding + "\n".join(shifted_lines)
 
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}o.o{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-        {DARK_GRAY}/ | \{RESET}
-       {GRAY}/__|__\{RESET}
-""",
 
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}^.^{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-       {DARK_GRAY}/  |  \{RESET}
-      {GRAY}/___|___\{RESET}
-""",
+SEAL_FRAMES = [
+    # Center
+    shift_art(SEAL_ART, 0, 0),
 
-    r"""
-       {GRAY}/\___/\{RESET}
-      {GRAY}( {WHITE}o.o{GRAY} ){RESET}
-      {DARK_GRAY}/|{GRAY}___{DARK_GRAY}|\{RESET}
-     {DARK_GRAY}/ |     | \{RESET}
-       {GRAY}/|___|\{RESET}
-      {DARK_GRAY}/  |  \{RESET}
-     {GRAY}/___|___\{RESET}
-""",
+    # Move slightly right
+    shift_art(SEAL_ART, 1, 0),
+
+    # Move right + slightly down
+    shift_art(SEAL_ART, 2, 1),
+
+    # Furthest point
+    shift_art(SEAL_ART, 3, 0),
+
+    # Move back
+    shift_art(SEAL_ART, 2, 0),
+
+    # Move slightly left
+    shift_art(SEAL_ART, 1, 1),
+
+    # Back to center
+    shift_art(SEAL_ART, 0, 0),
+
+    # Slightly left
+    shift_art(SEAL_ART, 1, 0),
+
+    # Slightly right
+    shift_art(SEAL_ART, 2, 1),
+
+    # Return
+    shift_art(SEAL_ART, 1, 0),
 ]
 
 
 MASCOTS = {
-    "fox": FOX_FRAMES,
-    "raccoon": RACCOON_FRAMES,
+    "seal": SEAL_FRAMES,
 }
