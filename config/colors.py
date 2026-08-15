46bdd1393561182dd34a7c5903042ad1402318ff
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-15 23:27:46 +0330

    Adding more colors

--- config/colors.py
+++ config/colors.py
@@ -10,8 +10,23 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 MAGENTA = "\033[35m"
 CYAN = "\033[36m"
+
 BRIGHT_MAGENTA = "\033[95m"
 BRIGHT_RED = "\033[91m"
+
+
+# 256-color ANSI codes
+
+ORANGE = "\033[38;5;208m"
+DARK_ORANGE = "\033[38;5;166m"
+
+LIGHT_GRAY = "\033[38;5;250m"
+GRAY = "\033[38;5;245m"
+DARK_GRAY = "\033[38;5;240m"
+
+WHITE = "\033[97m"
+BLACK = "\033[30m"
+
 
 MODE_COLORS = {
     "article": "#1f77b4",
