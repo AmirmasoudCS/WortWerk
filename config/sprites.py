96a924329fb108e06576ec155060356d068ad2f7
Amirmasoud <aammiirrrmasoud@gmail.com>
2026-08-15 23:24:05 +0330

    Implementing the sprites frames

---
+++ config/sprites.py
@@ -0,0 +1,30 @@
+FOX_FRAMES = [
+    r"""
+        /\   /\
+       (  ._. )
+        > ^ <
+    """,
+    r"""
+        /\   /\
+       (  o.o )
+        > ^ <
+    """,
+]
+
+RACCOON_FRAMES = [
+    r"""
+       (\_/)
+      ( ..  )
+      / >o
+    """,
+    r"""
+       (\_/)
+      ( -.- )
+      / >o
+    """,
+]
+
+MASCOTS = {
+    "fox": FOX_FRAMES,
+    "raccoon": RACCOON_FRAMES,
+}
